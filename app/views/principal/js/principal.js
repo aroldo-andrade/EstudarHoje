51b1d8f46ada7112538a18f962d4b03ce1fb4ab2
aroldo <aroldo.andrade0721@gmail.com>
2019-12-23 00:45:52 -0300

    tela principal trocando de dias da semana + inicio tela configuração

--- app/views/principal/js/principal.js
+++ app/views/principal/js/principal.js
@@ -1,1 +1,52 @@
+
+const {ipcRenderer} = require('electron')
 window.ELECTRON_DISABLE_SECURITY_WARNINGS = true
+
+
+let diaSemana = new Date().getDay()
+let diaDaSemanaImg = document.querySelector('.diaDaSemana')
+let cogA = document.querySelector('.cog-a')
+let content = document.querySelector('.content')
+let pathImgns = '../../res/imgs/$dia$.png'
+//diaSemana = 0
+
+
+cogA.addEventListener('click',()=>{
+    ipcRenderer.send('abrir-configuracao')
+})
+
+
+switch (diaSemana) {
+    case 0:
+        diaDaSemanaImg.src = pathImgns.replace('$dia$', 'domingo')
+        content.style.backgroundColor = '#025d08'
+        break;
+    case 1:
+        diaDaSemanaImg.src = pathImgns.replace('$dia$', 'segunda')
+        content.style.backgroundColor = '#6d0012'
+        break;
+    case 2:
+        diaDaSemanaImg.src = pathImgns.replace('$dia$', 'terca')
+        content.style.backgroundColor = '#b84900'
+        break;
+    case 3:
+        diaDaSemanaImg.src = pathImgns.replace('$dia$', 'quarta')
+        content.style.backgroundColor = '#ea6000'
+        break;
+    case 4:
+        diaDaSemanaImg.src = pathImgns.replace('$dia$', 'quinta')
+        content.style.backgroundColor = '#cbc200'
+        break;
+    case 5:
+        diaDaSemanaImg.src = pathImgns.replace('$dia$', 'sexta')
+        content.style.backgroundColor = '#92b915'
+        break;
+    case 6:
+        diaDaSemanaImg.src = pathImgns.replace('$dia$', 'sabado')
+        content.style.backgroundColor = '#07b70f'
+        break;
+
+    default:
+        break;
+}
+
--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-const { app,BrowserWindow } = require('electron')
+const { app,BrowserWindow, ipcMain } = require('electron')
 
 
 let mainWindow
-let devtoolWidth = 373
+let devtoolWidth = 0 //  rmeover apos o desenvolvimento
 app.on('ready',()=>{
 
     mainWindow = new BrowserWindow({
@@ -13,11 +13,41 @@ app.on('ready',()=>{
             nodeIntegration:true,
             devTools:true
         },
+        alwaysOnTop:true,
         x:1500, // ajudar aqui onde vc quer que a tela apareça primeiro
-        y:300
+        y:300,
+        maximizable:false,
     })
     mainWindow.loadFile('app/views/principal/principal.html')
+    if(devtoolWidth>0)
      mainWindow.webContents.openDevTools()
 
 })
+let configuracaoWin = null
+ipcMain.on('abrir-configuracao',()=>{
+    if(configuracaoWin == null){
+        configuracaoWin = new BrowserWindow({
+            width:250+devtoolWidth, 
+            height:300,
+            frame:false,
+            webPreferences:{
+                nodeIntegration:true,
+                devTools:true
+            },
+            alwaysOnTop:true,
+            x:1500, // ajudar aqui onde vc quer que a tela apareça primeiro
+            y:300,
+            maximizable:false,
+        });
+        configuracaoWin.on('closed', () => {
+            configuracaoWin = null;
+        })
+    }else{
+        configuracaoWin.focus()
+    }
+    configuracaoWin.loadFile(`app/views/configuracao/configuracao.html`);
+    if(devtoolWidth>0)
+    configuracaoWin.webContents.openDevTools()
 
+
+})
